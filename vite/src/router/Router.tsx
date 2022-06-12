9988867a3028224053d07fb5722d85bd600475a0
Nathan Folsom <n8folsom@gmail.com>
2022-06-12 18:48:28 -0400

    lazy load route components

--- vite/src/router/Router.tsx
+++ vite/src/router/Router.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ComponentType, lazy, Suspense, useMemo } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import buildRoutes from "./buildRoutes";
 
@@ -15,10 +15,16 @@ export default function Router({  }: RouterProps) {
 }
 
 function AppRoutes() {
-  const location = useLocation();
   return (
     <Routes>
-      {buildRoutes().map(r => <Route key={r.path} path={r.path} element={<r.Component location={location} />} />)}
+      {buildRoutes().map(r => <Route key={r.path} path={r.path} element={<LazyElement lazyEl={r.lazyComponent} />} />)}
     </Routes>
   );
+}
+
+function LazyElement({ lazyEl }: { lazyEl: () => Promise<{ default: ComponentType<{ location: any }> }>}) {
+  const location = useLocation();
+  const El = useMemo(() => lazy(lazyEl)
+  , []);
+  return <Suspense><El location={location} /></Suspense>
 }
--- vite/src/router/buildRoutes.ts
+++ vite/src/router/buildRoutes.ts
@@ -1,17 +1,11 @@
-import { FunctionComponent } from "react";
-
-export default function buildRoutes(): { path: string, Component: FunctionComponent<{ location: any }> }[] {
-  const modules = Object.entries(import.meta.globEager("../pages/**/*"))
-    .map(([k,v ]) => ({
+export default function buildRoutes(): { path: string, lazyComponent: any }[] {
+  return Object.entries(import.meta.glob("../pages/**/*"))
+    .map(([k, v]) => ({
       path: k
         .replace("../pages", "")
         .replace("index.tsx", "")
         .replace("[item].tsx", ":item")
         .replace("404.tsx", "*"),
-      Component: v.default
+      lazyComponent: v
     }));
-
-  console.log(modules)
-
-  return modules;
 }
